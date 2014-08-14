c68be2f9bf607330f38e88d2d3c7fcfc02866860
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-08-14 14:32:48 -0500

    Adds ability to show image from /tmp file

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 	  console.log("Request for" + pathname + " received.");
 
-		route(handle, pathname);	  
+	  request.addListener("data", function(postDataChunk) {
+	  	postData += postDataChunk;
+	  	console.log("Received POST data chunk '" +
+	  		postDataChunk + "'.");
+	  });
 
-	  response.writeHead(200, {"Content-Type": "text/plain"});
-	  response.write("Hello World " + pathname);
-	  response.end();
+	  request.addListener("end", function() {
+	  	route(handle, pathname, response, postData);
+	  });	  
 	}
+
 	http.createServer(onRequest).listen(9000);
 	console.log("Server has started.");
 }
