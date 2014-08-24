8846f4b2c8a65b073d49dc4b0f2b35b5608d1d7e
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-08-24 16:16:23 -0500

    Uuse case completed

--- requestHandlers.js
+++ requestHandlers.js
@@ -22,14 +22,25 @@ function start(response) {
 		response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
 
 	var form = new formidable.IncomingForm();
-	console.log("about to parse")
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done");
+
+	fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+		if (error) {
+			fs.unlink("/tmp/test.png");
+			fs.rename(files.upload.path, "/tmp/test.png");
+		}
+	});
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent:" + querystring.parse(postData).text);
+	response.write("Received image: <br/>");
+	response.write("<img src='/show' />");
 	response.end();
+	});
 }
 
 function show(response) {
